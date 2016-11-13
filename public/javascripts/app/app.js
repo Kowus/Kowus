59de4acd215b35f14a86bf6c19f1487e9cff0253
Kowus <barnabas@elite-education.org>
2016-11-13 17:25:01 +0000

    Cleaned Up Activity List

--- public/javascripts/app/app.js
+++ public/javascripts/app/app.js
@@ -17,27 +17,6 @@
             description: 'www.oususvchi.com',
             image: '/images/oususvchi.jpg',
             url: 'http://www.oususvchi.com'
-        }, {
-            name: 'Hour Of Code',
-            description: 'Some Description',
-            image: '/images/home.jpg',
-            url: 'http://www.oususvchi.com'
-        }, {
-            name: 'WOEP',
-            description: 'Some Description',
-            image: '/images/work.jpg',
-            url: 'http://www.oususvchi.com'
-        }, {
-            name: 'Calabule',
-            description: 'Some Calabule',
-            image: '/images/work.jpg',
-            url: 'http://www.oususvchi.com'
-        },
-        {
-            name: 'Poker',
-            description: 'Hello World!',
-            image: '/images/about.jpg',
-            url: 'Kowus.xyz'
         }
 
     ];
