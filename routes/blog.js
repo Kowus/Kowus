f4fe779e1ff836e7c36f3ee02eecf2f6a40320f4
Kowus <barnabas@elite-education.org>
2016-12-02 04:19:07 +0000

    Totally resolved Heroku bug

--- routes/blog.js
+++ routes/blog.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 var mongoose = require('mongoose');
 var Blog = require('./blog.model');
-var db = 'mongodb://localhost/blogDB';
+var db = 'mongodb://localhost/blogdb';
 
 mongoose.connect(db);
 
