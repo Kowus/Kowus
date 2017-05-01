3f8ea7c1fb9eb8eb7d8ae07cf8a154ee17ca89ad
kowus <barnabas@elite-education.org>
2017-05-01 12:34:38 +0000

    changed home image

--- routes/events.js
+++ routes/events.js
@@ -1,8 +1,13 @@
 var express = require('express');
 var router = express.Router();
+var countries = require('../public/templates/countries.json')
 
 router.get('/', function (req, res, next) {
-    res.render('events')
+    console.log(countries);
+    res.render('events',{
+        title: "Barnabas Nomo:: Events",
+        countries:countries
+    })
 });
 
 module.exports = router;
