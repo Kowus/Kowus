dfb8c2dae879abf2ae274d6c1f3e51c7593612d6
kowus <barnabas@elite-education.org>
2017-05-04 16:26:33 +0000

    added regional country codes

--- routes/events.js
+++ routes/events.js
@@ -1,12 +1,12 @@
 var express = require('express');
 var router = express.Router();
-var countries = require('../public/templates/countries.json')
+var countries = require('./countries.json');
 
 router.get('/', function (req, res, next) {
-    console.log(countries);
+
     res.render('events',{
         title: "Barnabas Nomo:: Events",
-        countries:countries
+        countries:Object.entries(countries)
     })
 });
 
