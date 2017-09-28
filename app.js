562c9a87850785f0e352549b38bd0278f73ec335
Kowus <barnabas@elite-education.org>
2017-09-28 02:00:29 +0000

    session resave

--- app.js
+++ app.js
@@ -71,7 +71,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(session({secret:process.env.SESSION_SECRET}));
+app.use(session({secret:process.env.SESSION_SECRET, resave:true, saveUninitialized:true}));
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(flash());
